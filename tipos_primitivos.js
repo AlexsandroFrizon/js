310ed3d58480b95343ed1e380f8b2dd9eb4d2484
AlexsandroFrizon <alexsandrofrizon21@gmail.com>
2021-08-02 22:46:36 -0300

    novos arquivos

---
+++ tipos_primitivos.js
@@ -0,0 +1,11 @@
+let nome = 'Alexsandro'; // String
+let idade = 20; // nuber literal 
+let logico = true; //boolean 
+let sobrenome = undefined; //undefined 
+let corSelecionada = null // Redefinir valor
+
+document.write(nome);
+document.write(idade);
+document.write(logico);
+document.write(sobrenome);
+document.write(corSelecionada);
---
+++ variaveis.js
@@ -0,0 +1,7 @@
+let idade = 20;
+let altura = 1.80;
+let nome = 'Alexsandro';
+
+document.write("Olá " + nome + ", tudo bem?");
+document.write('Sua altura é: ' + altura);
+document.write('Sua idade é: ' + idade);
