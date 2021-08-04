f41a7b5f2ba87a4396c1b9ca5e0b83e69f204cd5
AlexsandroFrizon <alexsandrofrizon21@gmail.com>
2021-08-03 22:33:07 -0300

    Como criar uma classe

---
+++ objeto.js
@@ -0,0 +1,12 @@
+//Declaração de atributos
+let nome = 'Alexsandro';
+let idade  = 20
+let altura = 1.80
+
+//Criando um objeto (classe?)
+let pessoa {
+	nome: 'Alexsandro'
+}
+
+//Imprimindo no console
+console.log(pessoa);
